86f7bf213371b5e4f4434d2ab6429d121dc6e49e
eloranta <hannu.eloranta@gmail.com>
2023-03-21 22:41:16 +0200

    add: reading and writing to eeprom

--- eepromprogrammer/eepromprogrammer.ino
+++ eepromprogrammer/eepromprogrammer.ino
@@ -1,12 +1,42 @@
 void setup()
 {
+  for (int pin = 2; pin < 10; pin++)
+    pinMode(pin,  INPUT);
+
   pinMode(13,  OUTPUT);
+  digitalWrite(13, HIGH);
+
+  delay(1000);
+ 
+  Serial.begin(57600);
+
+  byte data = 0;
+  for (int pin = 9; pin > 2; pin--)
+    data = (data << 1) + digitalRead(pin);
+
+  char buffer[80];
+  sprintf(buffer, "%x", data);
+  Serial.println(buffer);
+
+  for (int pin = 2; pin < 10; pin++)
+    pinMode(pin,  OUTPUT);
+
+  data = 7;
+  for (int pin = 2; pin < 10; pin++)
+  {
+    digitalWrite(pin, data & 1);
+    data = data >> 1;
+  }
+
+  digitalWrite(13, LOW);
+  delayMicroseconds(1);
+  digitalWrite(13, HIGH);
+
+  delay(1000);
+
 }
 
 void loop()
 {
-  digitalWrite(13, HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
-  delay(1000);
+
 }
